8f0bd04535fef5b017c34004bc31e72d3fbcd4ab
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-12 18:49:45 +0530

    REFACTORED: Method to search for a person by city and state

--- src/com/bridgelabz/addressbooksystem/AddressBookDirectory.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookDirectory.java
@@ -16,7 +16,7 @@ public class AddressBookDirectory {
 		do {
 
 			System.out.println("\nChoose the operation on the Directory you want to perform");
-			System.out.println("1.Add an Address Book\n2.Edit Existing Address Book\n3.Display Address book Directory\n4.Exit Address book System");
+			System.out.println("1.Add an Address Book\n2.Edit Existing Address Book\n3.Search Person By City\n4.Search Person By State\n5.Display Address book Directory\n6.Exit Address book System");
 
 			switch (scannerObject.nextInt()) {
 			case 1:
@@ -26,9 +26,15 @@ public class AddressBookDirectory {
 				editAddressBook();
 				break;
 			case 3:
-				displayDirectoryContents();
+				searchByCity();
 				break;
 			case 4:
+				searchByState();
+				break;
+			case 5:
+				displayDirectoryContents();
+				break;
+			case 6:
 				moreChanges = false;
 				System.out.println("Exiting Address Book Directory !");
 			}
@@ -64,6 +70,46 @@ public class AddressBookDirectory {
 			System.out.println("Book Does Not Exist");
 		}
 
+	}
+	
+	public void searchByCity() {
+		
+		System.out.println("Enter the name of the City where the Person resides : ");
+		String cityName = scannerObject.next();
+		System.out.println("Enter the name of the Person : ");
+		String personName = scannerObject.next();
+		
+		for(AddressBook addressBook : addressBookDirectory.values()) {
+			for(ContactPerson person : addressBook.getContact()) {
+				if(person.getFirstName().equals(personName) && person.getAddress().getCity().equals(cityName)) {
+					System.out.println(personName+" Found in Address Book : "+addressBook.getAddressBookName()+" !");
+					System.out.println(person);
+					return;
+				}
+			}
+		}
+		System.out.println("Contact Does Not Exist !!");
+		
+	}
+	
+	public void searchByState() {
+		
+		System.out.println("Enter the name of the State where the Person resides : ");
+		String StateName = scannerObject.next();
+		System.out.println("Enter the name of the Person : ");
+		String personName = scannerObject.next();
+		
+		for(AddressBook addressBook : addressBookDirectory.values()) {
+			for(ContactPerson person : addressBook.getContact()) {
+				if(person.getFirstName().equals(personName) && person.getAddress().getState().equals(StateName)) {
+					System.out.println(personName+" Found in Book : "+addressBook.getAddressBookName()+" !");
+					System.out.println(person);
+					return;
+				}
+			}
+		}
+		System.out.println("Contact Does Not Exist !!");
+		
 	}
 
 	public void displayDirectoryContents() {
