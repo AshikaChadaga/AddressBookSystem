6c7a05333353b7f541a7621217f05cc486807b52
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-14 19:25:17 +0530

    REFACTORED: Search Person by Region using Java Streams API

--- src/com/bridgelabz/addressbooksystem/AddressBookDirectory.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookDirectory.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -72,7 +73,7 @@ public class AddressBookDirectory {
 
 	}
 	
-	public void searchByCity() {
+public void searchByCity() {
 		
 		System.out.println("Enter the name of the City where the Person resides : ");
 		String cityName = scannerObject.next();
@@ -80,36 +81,27 @@ public class AddressBookDirectory {
 		String personName = scannerObject.next();
 		
 		for(AddressBook addressBook : addressBookDirectory.values()) {
-			for(ContactPerson person : addressBook.getContact()) {
-				if(person.getFirstName().equals(personName) && person.getAddress().getCity().equals(cityName)) {
-					System.out.println(personName+" Found in Address Book : "+addressBook.getAddressBookName()+" !");
-					System.out.println(person);
-					return;
-				}
-			}
-		}
-		System.out.println("Contact Does Not Exist !!");
-		
+			ArrayList<ContactPerson> contactList = addressBook.getContact();
+			contactList.stream()
+				.filter(person -> person.getFirstName().equals(personName) && person.getAddress().getCity().equals(cityName))
+				.forEach(person -> System.out.println(person));	
+		}		
 	}
 	
 	public void searchByState() {
 		
 		System.out.println("Enter the name of the State where the Person resides : ");
-		String StateName = scannerObject.next();
+		String stateName = scannerObject.next();
 		System.out.println("Enter the name of the Person : ");
 		String personName = scannerObject.next();
 		
 		for(AddressBook addressBook : addressBookDirectory.values()) {
-			for(ContactPerson person : addressBook.getContact()) {
-				if(person.getFirstName().equals(personName) && person.getAddress().getState().equals(StateName)) {
-					System.out.println(personName+" Found in Book : "+addressBook.getAddressBookName()+" !");
-					System.out.println(person);
-					return;
-				}
-			}
+			ArrayList<ContactPerson> contactList = ((AddressBook) addressBook).getContact();
+			contactList.stream()
+				.filter(person -> person.getFirstName().equals(personName) && person.getAddress().getState().equals(stateName))
+				.forEach(person -> System.out.println(person));		
 		}
-		System.out.println("Contact Does Not Exist !!");
-		
+
 	}
 
 	public void displayDirectoryContents() {
